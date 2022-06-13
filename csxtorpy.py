e203655ba292efe26ce17073113cf6ff4b75a045
Johoski <hanssenoner@gmail.com>
2022-06-13 11:05:08 +0200

    sus

--- csxtorpy.py
+++ csxtorpy.py
@@ -29,19 +29,14 @@ ascii_art = ("""
 
 print(ascii_art)                                                                                                                                              
 # reads data from txt
-while True:
-    filename1 = input("Name of the text file to read: ")
-    if not re.match(".+\.txt", filename1):
-        print ("Error! Make sure you select a txt file.")
-    else:
-        filename2 = input("Name of the output text file: ")
-        file2 = open(filename2, "w")
-        with open(filename1, "r") as file1:
-            while (line := file1.readline()):
-                patterneng = re.sub("\[EN\d+\]\s*",' "x "', line)
-                announcement = re.sub("\[Announcment+\]\s*", 'an "', patterneng)
-                haruka = re.sub("\[Haruka+\]\s*",'h "', announcement)
-                sora = re.sub("\[Sora+\]\s*",'s "', haruka)
-                file2.writelines(sora)
-                print(sora)
-                break
+filename1 = input("Name of the text file to read: ")
+file2 = open("output.txt", "w")
+with open(filename1, "r") as file1:
+    while (line := file1.readline()):
+        print(line)
+        patterneng = re.sub("\[EN\d+\]\s*",' "x "', line)
+        announcement = re.sub("\[Announcment+\]\s*", 'an "', patterneng)
+        haruka = re.sub("\[Haruka+\]\s*",'h "', announcement)
+        sora = re.sub("\[Sora+\]\s*",'s "', haruka)
+        file2.writelines(sora)
+        print(sora)
